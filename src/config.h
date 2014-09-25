732be758917e7a088b09d1e285e53cd7003e70e0
ravindramnath <ravindr@discovery.co.za>
2014-09-25 17:09:15 +0200

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TIME_FRAME          (GRect(0, 74, 144, 54))
+#define TIME_HOUR_FRAME     (GRect(0, 74, 65, 54))
+#define TIME_SEP_FRAME      (GRect(65, 70, 13, 54))
+#define TIME_MIN_FRAME      (GRect(79, 74, 66, 54))
+
+#define DATE_FRAME          (GRect(0, 113, 144, 30))
+#define DAY_FRAME           (GRect(0, 135, 144, 30))
+  
+#define WEATHER_ICON_FRAME  (GRect(18, 2, 50, 50))
+#define WEATHER_TEMP_FRAME  (GRect(61, 7, 83, 50))  
+#define WEATHER_HL_FRAME    (GRect(0, 43, 144, 30))
+
+#endif
